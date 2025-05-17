6b500be91ceb0bb3c30addf352995d8dcb59e882
jeff-zucker <dubzed@gmail.com>
2025-05-17 15:39:41 -0700

    version 3.0

--- dist/esm/NssAuthSession.d.ts
+++
@@ -1,8 +0,0 @@
-import { ILoginOptions } from ".";
-import { IAuthSession, INssSession } from "./IAuthSession";
-export declare class NssAuthSession implements IAuthSession {
-    session: INssSession;
-    authFetcher: any;
-    login(options: ILoginOptions, appUrl: string): Promise<unknown>;
-    _getAuthFetcher(options: ILoginOptions, appUrl: string, callback: Function): Promise<any>;
-}
